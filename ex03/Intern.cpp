ea79e806f0934c32a0ce7bcd1b510e925c4c9176
abdelkarim hajji <hajji.karim.solicode@gmail.com>
2024-08-13 13:25:43 +0100

    i remove some examples

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 10:44:07 by ahajji            #+#    #+#             */
-/*   Updated: 2024/04/29 14:29:40 by ahajji           ###   ########.fr       */
+/*   Updated: 2024/08/13 13:14:47 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,31 @@ Intern::Intern(const Intern& other) {
 }
 Intern::~Intern() {
 }
+
 Intern& Intern::operator=(const Intern& other) {
     std::cout << "Intern Assignation operator called" << std::endl;
     if (this == &other)
 		return *this;
     return *this;
 }
+
 Form	*Intern::makeForm(const std::string form_to_create, const std::string target_for_form)
+{
+    Form *all_forms[] = {new ShrubberyCreationForm(target_for_form), new RobotomyRequestForm(target_for_form), new PresidentialPardonForm(target_for_form)};
+    std::string forms[] = {"PresidentialPardonForm", "RobotomyRequestForm", "ShrubberyCreationForm"};
+    
+    for (int i = 0; i < 3; i++)
     {
-        Form *all_forms[] = {new ShrubberyCreationForm(target_for_form), new RobotomyRequestForm(target_for_form), new PresidentialPardonForm(target_for_form)};
-        std::string forms[] = {"PresidentialPardonForm", "RobotomyRequestForm", "ShrubberyCreationForm"};
-
-        for (int i = 0; i < 3; i++)
+        if (form_to_create == forms[i])
         {
-            if (form_to_create == forms[i])
-            {
-                std::cout << "Intern creates " << form_to_create << " now" << std::endl;
-                delete all_forms[0], delete all_forms[1], delete all_forms[2];
-                return (all_forms[i]);
-            }   
-        }
-
-        delete all_forms[0], delete all_forms[1], delete all_forms[2];
-        std::cout << "Intern can not create a form called " << form_to_create << "\033[0m" << std::endl;
-        return (NULL);
+            std::cout << "Intern creates " << form_to_create << " now" << std::endl;
+            for (int j = 0; j < 3; j++)
+                if (j != i) delete all_forms[i];
+            return (all_forms[i]);
+        }   
     }
+
+    delete all_forms[0], delete all_forms[1], delete all_forms[2];
+    std::cout << "Intern can not create a form called " << form_to_create << "\033[0m" << std::endl;
+    return (NULL);
+}
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -17,59 +17,28 @@
 
 int main(void)
 {
-    // try
-    // {
-    //     Bureaucrat b("ahajji", 12);
-    //     // Form f("Form", 5, -5);
-    // }
-    // catch(const std::exception& e)
-    // {
-    //     std::cerr << e.what() << '\n';
-    // }
-    // Create a bureaucrat with grade 137
-// try
-// {
-
-// Bureaucrat bob("Bob", 120);
-
-// // Create a shrubbery creation Form with target "home"
-// ShrubberyCreationForm Form("home");
-
-// // Bob signs the Form
-// Form.beSigned(bob);
-
-// // Bob executes the Form
-// Form.execute(bob);
-// }
-// catch(const std::exception& e)
-// {
-//     std::cerr << e.what() << '\n';
-// }
+    
 Intern intern;
 
-    // Test creating a PresidentialPardonForm
     Form* form1 = intern.makeForm("PresidentialPardonForm", "Target1");
     if (form1 != NULL) {
         // Use form1
         delete form1;
     }
 
-    // Test creating a RobotomyRequestForm
     Form* form2 = intern.makeForm("RobotomyRequestForm", "Target2");
     if (form2 != NULL) {
         // Use form2
         delete form2;
     }
 
-    // Test creating a ShrubberyCreationForm
     Form* form3 = intern.makeForm("ShrubberyCreationForm", "Target3");
     if (form3 != NULL) {
         // Use form3
         delete form3;
     }
 
-    // Test creating a form that doesn't exist
-    Form* form4 = intern.makeForm("NonexistentForm", "Target4");
+    Form* form4 = intern.makeForm("nonexistentform", "Target4");
     if (form4 != NULL) {
         // This should not happen
         delete form4;
