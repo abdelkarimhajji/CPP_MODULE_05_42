9ba33e740214bd620afe4db4aa65b46748b9a81d
abdelkarim hajji <hajji.karim.solicode@gmail.com>
2024-08-15 07:48:12 +0100

    i remover some tabs

--- ex03/RobotomyRequestForm.hpp
+++ ex03/RobotomyRequestForm.hpp
@@ -6,7 +6,7 @@
 /*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 17:46:28 by ahajji            #+#    #+#             */
-/*   Updated: 2024/08/13 15:33:14 by ahajji           ###   ########.fr       */
+/*   Updated: 2024/08/15 07:33:46 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 #include "Form.hpp"
 #include "Bureaucrat.hpp"
 
-// class Form;
 
 class RobotomyRequestForm : public Form
 {
